960593836cd3035fe905e464ff015f887981a4f8
José Fernando Gómez <jofergoz92@gmail.com>
2026-04-27 08:25:12 -0300

    carts-products

--- SRC/app.js
+++ SRC/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import productsRouter from "./routes/products.router.js";
+import cartsRouter from "./routes/carts.router.js";
 
 const app = express();
 const PORT = 8080;
@@ -6,6 +8,10 @@ const PORT = 8080;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Rutas
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 // Ruta de prueba
 app.get("/", (req, res) => {
